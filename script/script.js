6671d776899ca7c8432999b371fee531f5271d4c
MinaKamal <email>
2020-09-16 23:42:09 +0200

    added some visual enchancements to Register page

--- script/script.js
+++ script/script.js
@@ -1,27 +1,27 @@
 function emailvalid() {
     var email = document.getElementById("email").value;
     if (email.indexOf('.') == -1 || email.indexOf('@') == -1) {
-        document.getElementById("email").style="background-color: red";
+        document.getElementById("email").style="border-color: red";
     } else {
-        document.getElementById("email").style="background-color: green";
+        document.getElementById("email").style="border-color: green";
     }
 }
 
 function passvalid() {
     var pass = document.getElementById("createpass").value;
     if (pass.length < 8) {
-        document.getElementById("createpass").style="background-color: red";
+        document.getElementById("createpass").style="border-color: red";
     } else {
-        document.getElementById("createpass").style="background-color: green";
+        document.getElementById("createpass").style="border-color: green";
     }
 }
 function confirmvalid() {
     var pass = document.getElementById("createpass").value;
     var confirm = document.getElementById("confirmpass").value;
     if (pass == confirm) {
-        document.getElementById("confirmpass").style="background-color: green";
+        document.getElementById("confirmpass").style="border-color: green";
     } else {
-        document.getElementById("confirmpass").style="background-color: red";
+        document.getElementById("confirmpass").style="border-color: red";
     }
 }
 function checksubmit() {
@@ -30,13 +30,13 @@ function checksubmit() {
     var pass = document.getElementById("createpass").value;
     var confirm = document.getElementById("confirmpass").value;
     if (name == "") {
-        document.getElementById("usernameup").style="background-color: red";
+        document.getElementById("usernameup").style="border-color: red";
     } else if (email == "") {
-        document.getElementById("email").style="background-color: red";
+        document.getElementById("email").style="border-color: red";
     } else if (pass == "") {
-        document.getElementById("createpass").style="background-color: red";
+        document.getElementById("createpass").style="border-color: red";
     } else if (confirm == "") {
-        document.getElementById("confirmpass").style="background-color: red";
+        document.getElementById("confirmpass").style="border-color: red";
     } else {
         
     }
